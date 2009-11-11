dc5ac76752e522c5d6b6e3e65bbc9d39b8ee91fc
sni <sven@nierlein.de>
2009-11-11 19:30:16 +0000

    added examples
    git-svn-id: http://svn.nierlein.de/svn/perl/Nagios-Generator-TestConfig@67 0e3e1bab-e52c-46b4-ac39-a665d9bf5c4c

---
+++ examples/create_nagios_test_config.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use lib '../lib';
+use lib 'lib';
+use Nagios::Generator::TestConfig;
+my $ngt = Nagios::Generator::TestConfig->new(
+                    'output_dir' => '/opt/projects/nagios/nagios3-2.0/etc',
+                    'verbose'                   => 1,
+                    'overwrite_dir'             => 1,
+                    'hostcount'                 => 10,
+                    'services_per_host'         => 20,
+                    'normal_check_interval'     => 10,
+                    'retry_check_interval'      => 10,
+                    'nagios_cfg'                => [
+                            "broker_module=/tmp/check_mk-1.1.0beta10/livestatus.o /tmp/live.sock",
+                        ],
+);
+$ngt->create();
