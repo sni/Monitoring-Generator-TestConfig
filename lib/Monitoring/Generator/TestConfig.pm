d93b6b996e12997d12e00763202c49d39a6f43b7
Sven Nierlein <sven@nierlein.de>
2010-09-02 13:30:49 +0200

    released 0.32

--- lib/Monitoring/Generator/TestConfig.pm
+++ lib/Monitoring/Generator/TestConfig.pm
@@ -12,7 +12,7 @@ use Monitoring::Generator::TestConfig::HostCheckData;
 use Monitoring::Generator::TestConfig::InitScriptData;
 use Monitoring::Generator::TestConfig::P1Data;
 
-our $VERSION = '0.30';
+our $VERSION = '0.32';
 
 =head1 NAME
 
