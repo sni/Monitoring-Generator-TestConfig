28b78ece6b7c52e8c6621aba0396a4213ec5bc11
Sven Nierlein <sven@nierlein.de>
2021-02-23 17:09:11 +0100

    release 0.48

--- lib/Monitoring/Generator/TestConfig.pm
+++ lib/Monitoring/Generator/TestConfig.pm
@@ -14,7 +14,7 @@ use Monitoring::Generator::TestConfig::P1Data;
 use Monitoring::Generator::TestConfig::Modules::Shinken;
 use Monitoring::Generator::TestConfig::ShinkenInitScriptData;
 
-our $VERSION = '0.46';
+our $VERSION = '0.48';
 
 =head1 NAME
 
